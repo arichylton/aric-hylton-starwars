ee2389d0c3361ac536cc5e67d83332ceac20617e
Aric <arich212@gmail.com>
2024-12-05 17:10:46 -0600

    changed proxy

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,11 +2,8 @@ export const getStars = () => async (dispatch) => {
 	dispatch({ type: "GET_DATA_PENDING" })
 
 	try {
-		const response = await fetch(
-			"https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people/",
-		)
+		const response = await fetch("https://swapi.dev/api/people/")
 		const data = await response.json()
-
 		dispatch({ type: "GET_DATA", payload: data.results })
 	} catch (err) {
 		dispatch({ type: "GET_DATA_FAILURE", payload: err })
@@ -17,11 +14,8 @@ export const getFilms = () => async (dispatch) => {
 	dispatch({ type: "GET_FILMS_PENDING" })
 
 	try {
-		const response = await fetch(
-			"https://cors-anywhere.herokuapp.com/https://swapi.dev/api/films/",
-		)
+		const response = await fetch("https://swapi.dev/api/films/")
 		const data = await response.json()
-
 		dispatch({ type: "GET_FILMS", payload: data.results })
 	} catch (err) {
 		dispatch({ type: "GET_FILMS_FAILURE", payload: err })
@@ -32,13 +26,8 @@ export const getStarShips = () => async (dispatch) => {
 	dispatch({ type: "GET_STARSHIPS_PENDING" })
 
 	try {
-		const response = await fetch(
-			"https://cors-anywhere.herokuapp.com/https://swapi.dev/api/starships/",
-		)
+		const response = await fetch("https://swapi.dev/api/starships/")
 		const data = await response.json()
-
-		console.log(data.results)
-
 		dispatch({ type: "GET_STARSHIPS", payload: data.results })
 	} catch (err) {
 		dispatch({ type: "GET_STARSHIPS_FAILURE", payload: err })
