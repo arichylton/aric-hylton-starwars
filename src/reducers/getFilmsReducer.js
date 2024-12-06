2beb2e1d10bab8425715bfc0b3d59c498f502698
Aric <arich212@gmail.com>
2024-12-06 00:54:48 -0600

    updated resources in json

--- src/reducers/getFilmsReducer.js
+++ src/reducers/getFilmsReducer.js
@@ -1,19 +1,18 @@
 const initialStateData = {
-    isPending: false,
-    data: [],
-    error: ''
+	isPending: false,
+	data: [],
+	error: "",
 }
 
-
-export default (state=initialStateData, action={}) => {
-    switch (action.type) {
-        case 'GET_FILMS_PENDING':
-            return { ...state, isPending: true }
-        case 'GET_FILMS':
-            return { ...state, data: action.payload, isPending: false }
-        case 'GET_FILMS_FAILURE':
-            return { ...state, error: action.payload, isPending: false }
-        default:
-            return state;
-    }
+export default (state = initialStateData, action = {}) => {
+	switch (action.type) {
+		case "GET_FILMS_PENDING":
+			return { ...state, isPending: true }
+		case "GET_FILMS":
+			return { ...state, data: action.payload, isPending: false }
+		case "GET_FILMS_FAILURE":
+			return { ...state, error: action.payload, isPending: false }
+		default:
+			return state
+	}
 }
